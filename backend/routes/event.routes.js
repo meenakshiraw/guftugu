8eda05338d344d4d0f37b9eda70a949767fc244d
usmanaqadri <usmanaqadri@gmail.com>
2022-11-26 12:25:22 -0500

    cors working

--- backend/routes/event.routes.js
+++ backend/routes/event.routes.js
@@ -6,16 +6,16 @@ const ctrls = require("../controllers");
 // GET route to show INDEX of all events
 router.get("/event", ctrls.event.index);
 
-// GET route to SHOW a group's info
+// GET route to SHOW an event's info
 router.get("/event/:id", ctrls.event.show);
 
-// POST route to CREATE a new group
+// POST route to CREATE a new event
 router.post("/event", ctrls.event.create);
 
-// PUT route to UPDATE an existing group
+// PUT route to UPDATE an existing event
 router.put("/event/:id", ctrls.event.update);
 
-// DELETE route to destroy a group
+// DELETE route to destroy a event
 router.delete("/event/:id", ctrls.event.destroy);
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [eventList, setEventList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3009/event")
+      .then((res) => res.json())
+      .then((data) => setEventList(data.events));
+  }, []);
+
+  console.log("here is the event list!!", eventList);
+
   return (
     <div className="App">
       <Navbar />
